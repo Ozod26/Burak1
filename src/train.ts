1356ac3e302c6db4244047cc979c520877072000
Ozod26 <ozodfarid46@gmail.com>
2024-09-04 04:03:16 +0900

    feat: train vazifa bajarildi

--- src/train.ts
+++ src/train.ts
@@ -1,19 +1,35 @@
+/** H-TASK: 
+shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
+MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+ */
+
+function getPositive(arr: number[]): string {
+  const positiveNumbers = arr
+    .filter((num) => num > 0)
+    .map((num) => num.toString());
+
+  return positiveNumbers.join("");
+}
+
+const result = getPositive([1, -4, 2]);
+console.log(result);
+
 /**G-TASK: 
 Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
 MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
  */
 
-function getHighestIndex(numbers: string | any[]) {
-  let max = numbers[0];
-  let maxIndex = 0;
+// function getHighestIndex(numbers: string | any[]) {
+//   let max = numbers[0];
+//   let maxIndex = 0;
 
-  for (let i = 1; i < numbers.length; i++) {
-    if (numbers[i] > max) {
-      max = numbers[i];
-      maxIndex = i;
-    }
-  }
-  return maxIndex;
-}
-const result = getHighestIndex([5, 21, 12, 21, 8]);
-console.log(result);
+//   for (let i = 1; i < numbers.length; i++) {
+//     if (numbers[i] > max) {
+//       max = numbers[i];
+//       maxIndex = i;
+//     }
+//   }
+//   return maxIndex;
+// }
+// const result = getHighestIndex([5, 21, 12, 21, 8]);
+// console.log(result);
