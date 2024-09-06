576d90d0f3b1f5669462ab514c906261074a1474
Ozod26 <ozodfarid46@gmail.com>
2024-09-07 02:44:48 +0900

    feat: create routing and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import path from "path";
+import router from "./router";
 
 /** 1-ENTRANCE **/
 const app = express();
-console.log("__dirname:", __dirname);
+// console.log("__dirname:", __dirname);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,5 +16,6 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /** 4-ROUTERS  **/
+app.use("/", router); // Middlewere Design Pattern
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,9 @@ mongoose
     console.log("Mongodb connection succeed");
     const PORT = process.env.PORT ?? 3003;
     app.listen(PORT, function () {
-      console.log(`The server is running successfully on port: ${PORT}`);
+      console.log(
+        `The server is running successfully on port: ${PORT} http://localhost:3003`
+      );
     });
   })
   .catch((err) => console.log("ERROR on connection MongoDB", err));
