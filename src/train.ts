c58dc187dcfb237ec2bea8e69f7a23f4c4586e5b
Ozod26 <ozodfarid46@gmail.com>
2024-09-14 16:49:08 +0900

    fix: Task bajarildi

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,26 @@
+/** TASK J:
+
+Shunday function tuzing, u string qabul qilsin.
+Va string ichidagi eng uzun so'zni qaytarsin.
+
+MASALAN: findLongestWord("I came from Uzbekistan!"); return "Uzbekistan!"
+
+Yuqoridagi text tarkibida 'Uzbekistan'
+eng uzun so'z bo'lganligi uchun 'Uzbekistan'ni qaytarmoqda */
+
+function findLongestWord(sentence: string): string {
+  const words = sentence.match(/\b\w+\b/g) || [];
+
+  let longestWord = words.reduce((longest, current) => {
+    return current.length > longest.length ? current : longest;
+  }, "");
+
+  return longestWord;
+}
+
+const result = findLongestWord("I came from Uzbekistan!");
+console.log(result);
+
 /** TASK I:
 
 Shunday function tuzing, u parametrdagi array ichida eng ko'p
@@ -8,29 +31,29 @@ MASALAN: majorityElement([1, 2, 3, 4, 5, 4, 3, 4]); return 4
 Yuqoridag misolda argument sifatida kiritilayotgan array tarkibida
 4 soni ko'p takrorlanganligi uchun 4'ni return qilmoqda. */
 
-function majorityElement(arr: number[]): number | null {
-  const countMap: { [key: number]: number } = {};
-  let majorityElem: number | null = null;
-  let maxCount = 0;
+// function majorityElement(arr: number[]): number | null {
+//   const countMap: { [key: number]: number } = {};
+//   let majorityElem: number | null = null;
+//   let maxCount = 0;
 
-  for (const num of arr) {
-    if (countMap[num] === undefined) {
-      countMap[num] = 1;
-    } else {
-      countMap[num]++;
-    }
+//   for (const num of arr) {
+//     if (countMap[num] === undefined) {
+//       countMap[num] = 1;
+//     } else {
+//       countMap[num]++;
+//     }
 
-    if (countMap[num] > maxCount) {
-      maxCount = countMap[num];
-      majorityElem = num;
-    }
-  }
+//     if (countMap[num] > maxCount) {
+//       maxCount = countMap[num];
+//       majorityElem = num;
+//     }
+//   }
 
-  return majorityElem;
-}
+//   return majorityElem;
+// }
 
-const result = majorityElement([1, 2, 3, 4, 5, 4, 3, 4]);
-console.log(result);
+// const result = majorityElement([1, 2, 3, 4, 5, 4, 3, 4]);
+// console.log(result);
 
 /* Projcet Standards:
   - Logging standards
