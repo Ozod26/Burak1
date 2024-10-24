254cbfce58c8c3ed8351d4cffe964519759a1abb
Ozod26 <ozodfarid46@gmail.com>
2024-10-24 14:45:07 +0900

    feat: develop createOrder rest API

---
+++ src/models/Order.service.ts
@@ -0,0 +1,69 @@
+import OrderItemModel from "../schema/OrderItem.model";
+import OrderModel from "../schema/Order.model";
+import { Member } from "../libs/types/member";
+import { Order, OrderItemInput } from "../libs/types/order";
+import { shapeIntoMongooseObjectId } from "../libs/config";
+import Errors, { HttpCode, Message } from "../libs/Error";
+import { ObjectId } from "mongoose";
+
+class OrderService {
+  static createOrder(member: Member, body: any) {
+    throw new Error("Method not implemented.");
+  }
+  private readonly orderModel;
+  private readonly orderItemModel;
+
+  constructor() {
+    this.orderModel = OrderModel;
+    this.orderItemModel = OrderItemModel;
+  }
+
+  public async createOrder(
+    member: Member,
+    input: OrderItemInput[]
+  ): Promise<Order> {
+    // console.log("example input:", input);
+
+    const memberId = shapeIntoMongooseObjectId(member._id);
+
+    const amount = input.reduce((accumulator: number, item: OrderItemInput) => {
+      return accumulator + item.itemPrice * item.itemQuantity;
+    }, 0);
+
+    const delivery = amount < 100 ? 5 : 0;
+
+    // console.log("values:", amount, delivery);
+    try {
+      const newOrder: Order = await this.orderModel.create({
+        orderTotal: amount + delivery,
+        orderDelivery: delivery,
+        memberId: memberId,
+      });
+
+      const orderId = newOrder._id;
+      console.log("orderId:", newOrder._id);
+      await this.recordOrderItem(orderId, input);
+      return newOrder;
+    } catch (err) {
+      console.log("Error, model:createOrder:", err);
+      throw new Errors(HttpCode.BAD_REQUEST, Message.CREATED_FAILED);
+    }
+  }
+
+  private async recordOrderItem(
+    orderId: ObjectId,
+    input: OrderItemInput[]
+  ): Promise<void> {
+    const promisedList = input.map(async (item: OrderItemInput) => {
+      item.orderId = orderId;
+      item.productId = shapeIntoMongooseObjectId(item.productId);
+      await this.orderItemModel.create(item);
+      return "Inserted";
+    });
+
+    const orderItemState = await Promise.all(promisedList);
+    console.log("orderItemState:=>", orderItemState);
+  }
+}
+
+export default OrderService;
--- src/schema/Order.model.ts
+++ src/schema/Order.model.ts
@@ -25,7 +25,7 @@ const orderSchema = new Schema(
       ref: "Member",
     },
   },
-  { timestamps: true }
+  { timestamps: true, collection: "orders" }
 );
 
 export default mongoose.model("Order", orderSchema);
